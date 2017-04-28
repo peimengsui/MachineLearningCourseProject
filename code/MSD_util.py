aafca8547665f96d90751730f6737b1f6ac76cf4
Shi Fan <sf2632@nyu.edu>
2017-04-28 17:15:43 -0400

    updating util

--- code/MSD_util.py
+++ code/MSD_util.py
@@ -1,7 +1,7 @@
-import random, time, math
+import random, time, math, cPickle
 import sys, os
-import numpy as np
-from collections import defaultdict
+import numpy as np, pandas as pd
+from collections import defaultdict, Counter
 
 def get_user_song_pairs(_tr, _va):
     train_pairs, valid_pairs = defaultdict(set), defaultdict(set)
@@ -34,7 +34,64 @@ def song_to_count(if_str, binary=True):
     return stc
 
 def sort_dict_dec(d):
-    return sorted(d.keys(),key=lambda s:d[s],reverse=True)
+    return sorted(d.keys(), key=lambda s: d[s], reverse=True)
+
+def clean_songs():
+    stc = song_to_count("../data/train_triplets.txt", binary=True)
+    songs_to_keep = [song for song in stc if stc[song]>=10]
+    del stc
+    with open("../data/songs_to_keep.pkl", "wb") as output_file:
+        cPickle.dump(songs_to_keep, output_file)
+    with open("../data/songs_to_keep.pkl", "rb") as input_file:
+        songs_to_keep = cPickle.load(input_file)
+    train = pd.read_table("../data/train_data.txt", names=['user', 'song', 'count'], dtype={'count': str})
+    train = train[train['song'].isin(songs_to_keep)]
+    with open("../data/train_data_.txt", "w") as f:
+        f.write("\n".join(["\t".join(v) for v in train.values]))
+    print "train data processed"
+    valid_v = pd.read_table("../data/valid_visible.txt", names=['user', 'song', 'count'], dtype={'count': str})
+    valid_v = valid_v[valid_v['song'].isin(songs_to_keep)]
+    with open("../data/valid_visible_.txt", "w") as f:
+        f.write("\n".join(["\t".join(v) for v in valid_v.values]))
+    print "valid visible processed"
+    valid_p = pd.read_table("../data/valid_predict.txt", names=['user', 'song', 'count'], dtype={'count': str})
+    valid_p = valid_p[valid_p['song'].isin(songs_to_keep)]
+    with open("../data/valid_predict_.txt", "w") as f:
+        f.write("\n".join(["\t".join(v) for v in valid_p.values]))
+    print "valid predict processed"
+    test = pd.read_table("../data/test_data.txt", names=['user', 'song', 'count'], dtype={'count': str})
+    test = test[test['song'].isin(songs_to_keep)]
+    with open("../data/test_data_.txt", "w") as f:
+        f.write("\n".join(["\t".join(v) for v in test.values]))
+    print "test data processed"
+
+def clean_users():
+    with open("../data/songs_to_keep.pkl", "rb") as input_file:
+        songs_to_keep = cPickle.load(input_file)
+    train = pd.read_table("../data/train_triplets.txt", names=['user', 'song', 'count'], dtype={'count': str})
+    train = train[train['song'].isin(songs_to_keep)]
+    users_to_keep = [u[0] for u in Counter(list(train['user'])).most_common() if u[1]>=10]
+    print len(users_to_keep), "users to keep"
+    train = pd.read_table("../data/train_data_.txt", names=['user', 'song', 'count'], dtype={'count': str})
+    train = train[train['user'].isin(users_to_keep)]
+    with open("../data/train_data_.txt", "w") as f:
+        f.write("\n".join(["\t".join(v) for v in train.values]))
+    print "train data processed"
+    valid_v = pd.read_table("../data/valid_visible_.txt", names=['user', 'song', 'count'], dtype={'count': str})
+    valid_v = valid_v[valid_v['user'].isin(users_to_keep)]
+    with open("../data/valid_visible_.txt", "w") as f:
+        f.write("\n".join(["\t".join(v) for v in valid_v.values]))
+    print "valid visible processed"
+    valid_p = pd.read_table("../data/valid_predict_.txt", names=['user', 'song', 'count'], dtype={'count': str})
+    valid_p = valid_p[valid_p['user'].isin(users_to_keep)]
+    with open("../data/valid_predict_.txt", "w") as f:
+        f.write("\n".join(["\t".join(v) for v in valid_p.values]))
+    print "valid predict processed"
+    test = pd.read_table("../data/test_data_.txt", names=['user', 'song', 'count'], dtype={'count': str})
+    test = test[test['user'].isin(users_to_keep)]
+    with open("../data/test_data_.txt", "w") as f:
+        f.write("\n".join(["\t".join(v) for v in test.values]))
+    print "test data processed"
 
 def get_unique(if_str, users, songs):
     u, s = set(), set()
